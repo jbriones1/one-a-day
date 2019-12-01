2f6a4a01aed63202ec841f667e0ae51190e757b2
Jon Andre Briones <jbriones1@my.bcit.ca>
2019-11-30 16:50:35 -0800

    Added some JAYKWARY

---
+++ static/js/main.js
@@ -0,0 +1,53 @@
+function randNumber() {
+  taskColl.get().then(snap => {
+    let rand = parseInt(Math.random()*snap.size + 1);
+    return rand;
+    }
+  );
+}
+
+function loadTask() {
+  let tasks = [];
+  taskColl.get().then(snap => {
+    var rand = parseInt(Math.random()*snap.size);
+    snap.forEach(doc => {
+      var taskStr = doc.data().task;
+      tasks.push(taskStr);
+    });
+    console.log(tasks[rand]);
+    $("h1").html(tasks[rand]);
+  });  
+}
+
+function getUser() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log(user.displayName);
+      let firstname = user.displayName.split(' ')
+      document.getElementById("customizeHere").innerHTML = firstname[0];
+      db.collection("users").doc(user.uid).set({
+        "name": user.displayName,
+        "email": user.email,
+        "userid": user.uid,
+        "experience": 0,
+        "level": 1,
+        "last-tasks": [0, 0, 0]
+      },{merge: true});
+    } else {
+      console.log(user);
+    }
+  });
+}
+
+function createCollection() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      userColl.doc(user.uid).collection("past-tasks").doc("dummy").set({
+        "task ID": "dummy task",
+        "points": 0
+      },{merge: true});
+    }
+  })
+}
+
+

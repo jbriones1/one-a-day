88cdcfdab88e987a67d1003b514a29a82e68a117
Jon Andre Briones <jbriones1@my.bcit.ca>
2019-11-30 19:07:51 -0800

    Fixed all the front-end problems -Jon

--- static/js/main.js
+++ static/js/main.js
@@ -22,19 +22,15 @@ function loadTask() {
 function getUser() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      console.log(user.displayName);
-      let firstname = user.displayName.split(' ')
-      // document.getElementById("customizeHere").innerHTML = firstname[0];
-      db.collection("users").doc(user.uid).set({
-        "name": user.displayName,
-        "email": user.email,
-        "userid": user.uid,
-        "experience": 0,
-        "level": 1,
-        "last-tasks": [0, 0, 0]
-      },{merge: true});
-    } else {
-      console.log(user);
+      checkPercent(user);
+      const usersRef = userColl.doc(user.uid);
+      usersRef.get()
+        .then( snap => {
+          if (!snap.exists) {
+            createUser(user);
+          }
+        });
+      let firstname = user.displayName.split(' ');
     }
   });
 }
@@ -47,6 +43,47 @@ function createCollection() {
         "points": 0
       },{merge: true});
     }
+  })
+}
+
+function createUser(user) {
+  db.collection("users").doc(user.uid).set({
+    "name": user.displayName,
+    "email": user.email,
+    "userid": user.uid,
+    "experience": 0,
+    "level": 1,
+    "last-tasks": [0, 0, 0]
+  },{merge: true});
+}
+
+
+function buttonPress() {
+    
+  const increment = firebase.firestore.FieldValue.increment(1);
+  var user = firebase.auth().currentUser;
+  let exp = db.collection('users').doc(user.uid);
+  exp.update({ experience: increment});
+  updateExp();
+  console.log("pressed");
+}
+
+function updateExp() {
+  var user = firebase.auth().currentUser;
+  let exp = db.collection('users').doc(user.uid).onSnapshot(function (snap) {
+    let exp = snap.data().experience;
+    $("#numberOfPoints").html(exp);
+    checkPercent(user);
+  });
+}
+
+function checkPercent(user) {
+  let id = user.uid;
+  userColl.doc(id).onSnapshot(function (snap) {
+    let exp = snap.data().experience;
+    let perc = exp / 10 * 100;
+    console.log(perc);
+    $("#progressBar").css("width", perc+"%");
   })
 }
 
